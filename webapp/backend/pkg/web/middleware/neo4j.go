81a54493edd6669126c999a216674d6ed6efe765
Jason Kulatunga <jason@thesparktree.com>
2021-12-15 20:56:37 -0800

    logging neo4j

--- webapp/backend/pkg/web/middleware/neo4j.go
+++ webapp/backend/pkg/web/middleware/neo4j.go
@@ -12,7 +12,25 @@ func DatabaseMiddleware(appConfig config.Interface, globalLogger logrus.FieldLog
 	//var database *gorm.DB
 	fmt.Printf("Trying to connect to database stored: %s\n", appConfig.GetString("web.database.location"))
 
-	driver, err := neo4j.NewDriver(appConfig.GetString("web.database.uri"), neo4j.BasicAuth(appConfig.GetString("web.database.username"), appConfig.GetString("web.database.password"), appConfig.GetString("web.database.realm")))
+	useConsoleLogger := func(level neo4j.LogLevel) func(config *neo4j.Config) {
+		return func(config *neo4j.Config) {
+			config.Log = neo4j.ConsoleLogger(level)
+		}
+	}
+
+	neo4jLevel := neo4j.ERROR
+	if appConfig.GetString("log.level") == "DEBUG" {
+		neo4jLevel = neo4j.DEBUG
+	}
+
+	driver, err := neo4j.NewDriver(appConfig.GetString("web.database.uri"),
+		neo4j.BasicAuth(
+			appConfig.GetString("web.database.username"),
+			appConfig.GetString("web.database.password"),
+			appConfig.GetString("web.database.realm"),
+		),
+		useConsoleLogger(neo4jLevel),
+	)
 	if err != nil {
 		panic(fmt.Errorf("failed to connect to database! - %v", err))
 	}
